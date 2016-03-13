a36640c0df731c87a7cf84dc23f61caa7bc90283
Aleksey Zakrytnoy <a.zakrytnoy@gmail.com>
2016-03-13 18:38:35 +0100

    u468

---
+++ u468/u468.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class U468
+{
+public:
+	static const char * libname()
+	{
+		return "u468";
+	}
+    U468() {}
+
+    void operator()();
+};
+
+#ifdef POPULATE_CACHE
+populate <U468> pu468;
+#endif
