42ba3e3ac39396e13c242571dc95e225bcd0bfd5
Aleksey Zakrytnoy <aleksey.zakrytnoy@nl.abnamro.com>
2016-03-29 18:21:44 +0200

    fixing bigint

--- u324/u324.cpp
+++ u324/u324.cpp
@@ -21,73 +21,71 @@
 #include "bigint.h"
 
 extern "C" {
-    UVA_API_EXPORT void __cdecl invoke();
+	UVA_API_EXPORT void __cdecl invoke();
 }
 void __cdecl invoke()
 {
-    U324 instance;
-    instance();
+	U324 instance;
+	instance();
 }
 
-namespace  {
+namespace {
+	class solution {
+		std::vector<std::vector<uint16_t>> cache_;
+		uint16_t number_;
+	public:
+		solution() : cache_(), number_(0)
+		{
+			math::BigInteger fact("1");
+			cache_.resize(368);
+			for (uint16_t i = 1; i <= 367; i++) {
+				cache_[i].resize(10);
+				fact *= (int)i;
+				bool leading0(true);
+				std::for_each(fact.data().rbegin(), fact.data().rend(), [&](auto digit) {
+					if (leading0) {
+						leading0 = digit == 0;
+					}
+					if (!leading0) {
+						cache_[i][digit]++;
+					}
+				});
+				std::ostream_iterator<uint16_t> oit(std::clog, " ");
+				std::copy(cache_[i].begin(), cache_[i].end(), oit);
+				std::clog << " <- " << i << std::endl;
+			}
+		}
 
-class solution {
-    std::vector<std::vector<uint16_t>> cache_;
-    uint16_t number_;
-public:
-    solution() : cache_(), number_(0)
-    {
-        math::BigInteger fact ("1");
-        cache_.resize(367);
-        for (uint16_t i = 1; i <= 367; i++) {
-            cache_[i].resize(10);
-            fact *= (int)i;
-            bool leading0 (true);
-            std::for_each(fact.data().rbegin(), fact.data().rend(), [&](auto digit) {
-                if (leading0) {
-                    leading0 = digit == 0;
-                }
-                if (!leading0) {
-                    cache_[i][digit]++;
-                }
-            });
-            std::ostream_iterator<uint16_t> oit (std::clog, " ");
-            std::copy (cache_[i].begin(), cache_[i].end(), oit);
-            std::clog << " <- " << i << std::endl;
-        }
-    }
+		solution& operator () (uint16_t n)
+		{
+			number_ = n;
+			return *this;
+		}
 
-    solution& operator () (uint16_t n)
-    {
-        number_ = n;
-        return *this;
-    }
+		friend std::ostream& operator << (std::ostream& out, solution& sol);
 
-    friend std::ostream& operator << (std::ostream& out, solution& sol);
+	private:
+	};
 
-private:
-};
-
-std::ostream& operator << (std::ostream& out, solution& sol)
-{
-    out << sol.number_ << "! --";
-    uint16_t idx (0);
-    std::for_each(sol.cache_[sol.number_].begin(), sol.cache_[sol.number_].end(), [&](auto freq) {
-        if (idx % 5 == 0) {
-            out << std::endl;
-        }
-        out << "  (" << (idx++) << ")" << std::setw(3) << freq;
-    });
-    return out;
-}
-
+	std::ostream& operator << (std::ostream& out, solution& sol)
+	{
+		out << sol.number_ << "! --";
+		uint16_t idx(0);
+		std::for_each(sol.cache_[sol.number_].begin(), sol.cache_[sol.number_].end(), [&](auto freq) {
+			if (idx % 5 == 0) {
+				out << std::endl;
+			}
+			out << "  (" << (idx++) << ")" << std::setw(3) << freq;
+		});
+		return out;
+	}
 }  // namespace
 
 void U324::operator()()
 {
-    solution sol;
-    uint16_t n;
-    while (std::cin >> n && n > 0) {
-        std::cout << sol (n) << std::endl;
-    }
+	solution sol;
+	uint16_t n;
+	while (std::cin >> n && n > 0) {
+		std::cout << sol(n) << std::endl;
+	}
 }
