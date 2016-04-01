4813b0a640b14aa13a4fa86e7f8d8b8c0137c973
Aleksey Zakrytnoy <aleksey.zakrytnoy@nl.abnamro.com>
2016-04-01 17:08:54 +0200

    code cleanup

--- u10161/u10161.cpp
+++ u10161/u10161.cpp
@@ -17,58 +17,59 @@
 #include <limits>
 
 extern "C" {
-    UVA_API_EXPORT void __cdecl invoke();
+	UVA_API_EXPORT void __cdecl invoke();
 }
 void __cdecl invoke()
 {
-    U10161 instance;
-    instance();
+	U10161 instance;
+	instance();
 }
 
-namespace  {
+namespace {
+	class solution {
+		int x_;
+		int y_;
+	public:
+		solution() : x_(0), y_(0) {}
 
-class solution {
-    int x_;
-    int y_;
-public:
-    solution() : x_(0), y_(0) {}
+		friend std::ostream& operator << (std::ostream& out, const solution& sol)
+		{
+			out << sol.x_ << " " << sol.y_;
+			return out;
+		}
 
-    friend std::ostream& operator << (std::ostream& out, const solution& sol)
-    {
-        out << sol.x_ << " " << sol.y_;
-        return out;
-    }
+		solution& operator () (int n)
+		{
+			int root(ceil(sqrt(n)));
+			int shift(root * root - root + 1);
+			x_ = y_ = root;
 
-    solution& operator () (int n)
-    {
-        int root = ceil(sqrt(n));
-        int shift = root * root - root + 1;
-        x_ = y_ = root;
-
-        if(root % 2 == 0) {
-            if(n > shift) {
-                y_ -= n - shift;
-            } else {
-                x_ -= shift - n;
-            }
-        } else {
-            if(n > shift) {
-                x_ -= n - shift;
-            } else {
-                y_ -= shift - n;
-            }
-        }
-        return *this;
-    }
-};
-
+			if (root % 2 == 0) {
+				if (n > shift) {
+					y_ -= n - shift;
+				}
+				else {
+					x_ -= shift - n;
+				}
+			}
+			else {
+				if (n > shift) {
+					x_ -= n - shift;
+				}
+				else {
+					y_ -= shift - n;
+				}
+			}
+			return *this;
+		}
+	};
 }  // namespace
 
 void U10161::operator()()
 {
-    int n;
-    solution sol;
-    while (std::cin >> n && n != 0) {
-        std::cout << sol(n) << std::endl;
-    }
+	int n;
+	solution sol;
+	while (std::cin >> n && n != 0) {
+		std::cout << sol(n) << std::endl;
+	}
 }
--- u740/u740.cpp
+++ u740/u740.cpp
@@ -18,73 +18,71 @@
 #include <map>
 
 extern "C" {
-    UVA_API_EXPORT void __cdecl invoke();
+	UVA_API_EXPORT void __cdecl invoke();
 }
 void __cdecl invoke()
 {
-    U740 instance;
-    instance();
+	U740 instance;
+	instance();
 }
 
-namespace  {
+namespace {
+	class baudot {
+		enum class shift {
+			UP, DOWN
+		};
+		shift shift_;
+		std::map<shift, std::string> code_;
 
-class baudot {
-    enum class shift {
-        UP, DOWN
-    };
-    shift shift_;
-    std::map<shift, std::string> code_;
+	public:
+		baudot() : shift_(shift::DOWN), code_() {}
 
-public:
-    baudot() : shift_(shift::DOWN), code_() {}
+		friend std::istream& operator >> (std::istream& in, baudot& eng);
 
-    friend std::istream& operator >> (std::istream& in, baudot& eng);
+		friend std::string operator << (baudot& eng, const std::string& input);
+	};
 
-    friend std::string operator << (baudot& eng, const std::string& input);
-};
+	std::istream& operator >> (std::istream& in, baudot& eng)
+	{
+		eng.shift_ = baudot::shift::DOWN;
+		std::getline(in, eng.code_[baudot::shift::DOWN]);
+		std::getline(in, eng.code_[baudot::shift::UP]);
+		return in;
+	}
 
-std::istream& operator >> (std::istream& in, baudot& eng)
-{
-    eng.shift_ = baudot::shift::DOWN;
-    std::getline(in, eng.code_[baudot::shift::DOWN]);
-    std::getline(in, eng.code_[baudot::shift::UP]);
-    return in;
-}
-
-std::string operator << (baudot& eng, const std::string& input)
-{
-    eng.shift_ = baudot::shift::DOWN;
-    std::string result;
-    for (auto it = input.begin(); it != input.end(); ) {
-        uint16_t code(0);
-        for (size_t power = 0x10; power > 0; power >>= 1, ++it) {
-            if (*it == '1') {
-                code += power;
-            }
-        }
-        switch (code) {
-        case 0b11011:
-            eng.shift_ = baudot::shift::DOWN;
-            break;
-        case 0b11111:
-            eng.shift_ = baudot::shift::UP;
-            break;
-        default:
-            result += eng.code_[eng.shift_][code];
-            break;
-        }
-    }
-    return result;
-}
-
+	std::string operator << (baudot& eng, const std::string& input)
+	{
+		eng.shift_ = baudot::shift::DOWN;
+		std::string result;
+		for (auto it = input.begin(); it != input.end(); ) {
+			uint16_t code(0);
+			for (size_t power = 0x10; power > 0; power >>= 1, ++it) {
+				if (*it == '1') {
+					code += uint16_t(power);
+				}
+			}
+			switch (code) {
+			case 0b11011:
+				eng.shift_ = baudot::shift::DOWN;
+				break;
+			case 0b11111:
+				eng.shift_ = baudot::shift::UP;
+				break;
+			default:
+				result += eng.code_[eng.shift_][code];
+				break;
+			}
+		}
+		return result;
+	}
 }  // namespace
 
 void U740::operator()()
 {
-    baudot eng;
-    std::cin >> eng;
-    std::string line;
-    while (std::getline(std::cin, line) && !line.empty()) {
-        std::cout << (eng << line) << std::endl;
-    }
+	baudot eng;
+	std::cin >> eng;
+	std::string line;
+	while (std::getline(std::cin, line) && !line.empty()) {
+		std::cout << (eng << line) << std::endl;
+	}
 }
