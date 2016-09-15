3968e51f019985e0bf438ae0d2ec5cbe4fbd9b8f
alex.zakrytnoy <alex.zakrytnoy@objectplus.nl>
2016-09-15 15:31:06 +0200

    u10026

---
+++ u10026/u10026.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class U10026
+{
+public:
+	static const char * libname()
+	{
+		return "u10026";
+	}
+    U10026() {}
+
+    void operator()();
+};
+
+#ifdef POPULATE_CACHE
+populate <U10026> pu10026;
+#endif
