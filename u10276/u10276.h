d4bb8f701231bc293e40a1a4164d79bcbc52862d
Aleksey Zakrytnoy <aleksey.zakrytnoy@nl.abnamro.com>
2017-01-04 10:12:38 +0100

    u10276

---
+++ u10276/u10276.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class U10276 {
+public:
+    static const char* libname()
+    {
+        return "u10276";
+    }
+    U10276() {}
+
+    void operator()() const;
+};
+
+#ifdef POPULATE_CACHE
+    populate <U10276> pu10276;
+#endif
