43b3ff8bb1d368146be6b94510cb2244907d6190
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-03 14:37:49 +0100

    initial

---
+++ u726/u726.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class U726
+{
+public:
+	static const char * libname()
+	{
+		return "u726";
+	}
+    U726() {}
+
+    void operator()() const;
+};
+
+#ifdef POPULATE_CACHE
+populate <U726> pu726;
+#endif
