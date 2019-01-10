895e7a814abaabd83b0fe3b6d11b480fddfe8d70
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-10 12:55:55 +0100

    initial

---
+++ u11697/u11697.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class U11697
+{
+public:
+	static const char * libname()
+	{
+		return "u11697";
+	}
+    U11697() {}
+
+    void operator()() const;
+};
+
+#ifdef POPULATE_CACHE
+populate <U11697> pu11697;
+#endif
