0de72faa30009ce69ae15371ad2efc3bb1f87176
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-15 20:49:54 +0100

    initial

---
+++ u118/u118.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class U118
+{
+public:
+	static const char * libname()
+	{
+		return "u118";
+	}
+    U118() {}
+
+    void operator()() const;
+};
+
+#ifdef POPULATE_CACHE
+populate <U118> pu118;
+#endif
