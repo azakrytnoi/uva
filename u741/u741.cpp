cdb4f8a3f3265cedd02f11ddd1a36edc0cb66032
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-17 14:00:19 +0100

    done

--- u741/u741.cpp
+++ u741/u741.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u741.h"
@@ -17,52 +17,82 @@
 #include <limits>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U741 instance;
-	instance();
+    U741 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
+    class solution {
+    public:
+        solution() : message_(), position_(), next_() {}
 
-class solution
-{
-public:
-    solution() { }
+        solution& operator()()
+        {
+            std::string temp(message_);
+            std::sort(temp.begin(), temp.end());
+            next_.clear();
+            next_.resize(temp.size());
+            size_t last_pos(std::numeric_limits<size_t>::max());
 
-    friend std::istream& operator >>(std::istream& in, solution& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution& sol);
+            for (size_t idx = 0; idx < temp.size(); idx++) {
+                if (last_pos == std::numeric_limits<size_t>::max()) {
+                    auto pos = message_.find(temp[idx]);
+                    next_[idx] = last_pos = pos;
+                } else {
+                    if (temp[idx] == temp[idx - 1]) {
+                        auto pos = message_.find(temp[idx], last_pos + 1);
+                        last_pos = std::numeric_limits<size_t>::max();
+                        next_[idx] = last_pos = pos;
+                    } else {
+                        auto pos = message_.find(temp[idx]);
+                        next_[idx] = last_pos = pos;
+                    }
+                }
+            }
 
-    operator bool() const { return true; }
-    solution& operator()();
+            return *this;
+        }
 
-private:
-};
+        operator bool ()
+        {
+            return not(message_ == "END" && position_ == 0);
+        }
 
-std::istream& operator >> (std::istream& in, solution& sol)
-{
-  return in;
-}
+        friend std::istream& operator >> (std::istream& in, solution& sol)
+        {
+            in >> sol.message_ >> sol.position_;
+            return in;
+        }
 
-std::ostream& operator << (std::ostream& out, const solution& sol)
-{
-  return out;
-}
+        friend std::ostream& operator << (std::ostream& out, const solution& sol)
+        {
+            size_t pos = sol.position_ - 1;
 
-solution& solution::operator()()
-{
-  return *this;
-}
+            for (size_t idx = 0; idx < sol.message_.size(); idx++) {
+                pos = sol.next_[pos];
+                out << sol.message_[pos];
+            }
 
+            out << std::endl;
+            return out;
+        }
+
+    private:
+        std::string message_;
+        uint16_t position_;
+        std::vector<size_t> next_;
+    };
 }
 
 void U741::operator()() const
 {
     solution sol;
+
     while (std::cin >> sol && sol) {
         std::cout << sol() << std::endl;
     }
