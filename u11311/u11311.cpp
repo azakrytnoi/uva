91c730768c83a45c7e6fb102d07d6e670d9bd5be
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-27 16:35:55 +0100

    done

--- u11311/u11311.cpp
+++ u11311/u11311.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u11311.h"
@@ -17,52 +17,58 @@
 #include <limits>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U11311 instance;
-	instance();
+    U11311 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : N_(std::numeric_limits<size_t>::max()), m_(), n_(), r_(), c_() {}
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+        operator bool()
+        {
+            return N_-- > 0;
+        }
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+        solution_t& operator()()
+        {
+            return *this;
+        }
 
-private:
-};
+        friend std::istream& operator>>(std::istream& in, solution_t& sol)
+        {
+            if (sol.N_ == std::numeric_limits<size_t>::max()) {
+                in >> sol.N_;
+            }
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+            in >> sol.m_ >> sol.n_ >> sol.r_ >> sol.c_;
+            return in;
+        }
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+        friend std::ostream& operator<<(std::ostream& out, const solution_t& sol)
+        {
+            out << (((sol.m_ - sol.r_ - 1) ^ (sol.n_ - sol.c_ - 1) ^ sol.r_ ^ sol.c_) != 0 ? "Gretel" : "Hansel");
+            return out;
+        }
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+    private:
+        size_t N_;
+        size_t m_, n_, r_, c_;
+    };
 
 }
 
 void U11311::operator()() const
 {
     solution_t sol;
+
     while (std::cin >> sol && sol) {
         std::cout << sol() << std::endl;
     }
