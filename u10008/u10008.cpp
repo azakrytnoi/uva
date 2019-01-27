193f9da66353fa85fd431da55707264aef9cdbba
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-27 16:08:56 +0100

    done

--- u10008/u10008.cpp
+++ u10008/u10008.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u10008.h"
@@ -15,54 +15,90 @@
 #include <iterator>
 #include <numeric>
 #include <limits>
+#include <map>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U10008 instance;
-	instance();
+    U10008 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : source_(), freq_() {}
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+        operator bool()
+        {
+            return not source_.empty();
+        }
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+        solution_t& operator()()
+        {
+            freq_.clear();
+            std::map<char, size_t> counter;
 
-private:
-};
+            for (auto ch : source_) {
+                if (std::isalpha(ch)) {
+                    counter[std::toupper(ch)]++;
+                }
+            }
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+            freq_.assign(counter.begin(), counter.end());
+            std::sort(freq_.begin(), freq_.end(), [](const std::pair<char, size_t>& lhs, const std::pair<char, size_t>& rhs) {
+                if (lhs.second != rhs.second) {
+                    return lhs.second > rhs.second;
+                }
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+                return lhs.first < rhs.first;
+            });
+            return *this;
+        }
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+        friend std::istream& operator>>(std::istream& in, solution_t& sol)
+        {
+            sol.source_.clear();
+
+            if (in) {
+                size_t n(0);
+                in >> n;
+                std::string dummy;
+                std::getline(in, dummy);
+
+                while (n--) {
+                    std::getline(in, dummy);
+                    sol.source_ += dummy;
+                }
+            }
+
+            return in;
+        }
+
+        friend std::ostream& operator<<(std::ostream& out, const solution_t& sol)
+        {
+            for (auto& pair : sol.freq_) {
+                out << pair.first << ' ' << pair.second << std::endl;
+            }
+
+            return out;
+        }
+
+    private:
+        std::string source_;
+        std::vector<std::pair<char, size_t>> freq_;
+    };
 
 }
 
 void U10008::operator()() const
 {
     solution_t sol;
+
     while (std::cin >> sol && sol) {
         std::cout << sol() << std::endl;
     }
