d1346a7cf62e1b076ec4d94a7f1e00bcba12ece8
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-27 16:32:17 +0100

    initial

---
+++ u11311/u11311.cpp
@@ -0,0 +1,69 @@
+#ifdef _WIN32
+#define UVA_API_EXPORT __declspec(dllexport)
+#else
+#define __cdecl
+#define UVA_API_EXPORT
+#endif
+
+#include "u11311.h"
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <numeric>
+#include <limits>
+
+extern "C" {
+	UVA_API_EXPORT void __cdecl invoke();
+}
+
+void __cdecl invoke()
+{
+	U11311 instance;
+	instance();
+}
+
+namespace
+{
+
+class solution_t
+{
+public:
+    solution_t() { }
+
+    friend std::istream& operator >>(std::istream& in, solution_t& sol);
+    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+
+    operator bool() const { return true; }
+    solution_t& operator()();
+
+private:
+};
+
+std::istream& operator >> (std::istream& in, solution_t& sol)
+{
+  return in;
+}
+
+std::ostream& operator << (std::ostream& out, const solution_t& sol)
+{
+  return out;
+}
+
+solution_t& solution_t::operator()()
+{
+  return *this;
+}
+
+}
+
+void U11311::operator()() const
+{
+    solution_t sol;
+    while (std::cin >> sol && sol) {
+        std::cout << sol() << std::endl;
+    }
+}
