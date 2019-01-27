fd8bbe436fd33832a35dbc9f81dea2aee885d628
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-27 16:15:26 +0100

    done

--- u10165/u10165.cpp
+++ u10165/u10165.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u10165.h"
@@ -17,52 +17,63 @@
 #include <limits>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U10165 instance;
-	instance();
+    U10165 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : N_(), S_() {}
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+        operator bool()
+        {
+            return N_ != 0;
+        }
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+        solution_t& operator()()
+        {
+            return *this;
+        }
 
-private:
-};
+        friend std::istream& operator>>(std::istream& in, solution_t& sol)
+        {
+            uint64_t n;
+            in >> n;
+            sol.N_ = n;
+            sol.S_ = 0;
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+            while (n--) {
+                uint64_t t;
+                in >> t;
+                sol.S_ ^= t;
+            }
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+            return in;
+        }
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+        friend std::ostream& operator<<(std::ostream& out, const solution_t& sol)
+        {
+            out << (sol.S_ != 0 ? "Yes" : "No");
+            return out;
+        }
+
+    private:
+        uint64_t N_, S_;
+    };
 
 }
 
 void U10165::operator()() const
 {
     solution_t sol;
+
     while (std::cin >> sol && sol) {
         std::cout << sol() << std::endl;
     }
