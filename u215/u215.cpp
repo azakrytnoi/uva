a188f301289ac6e37bc7c238fded3e65dd8bad5f
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-01-31 22:51:39 +0100

    fixed calculation

--- u215/u215.cpp
+++ u215/u215.cpp
@@ -28,284 +28,327 @@ void __cdecl invoke()
 
 namespace {
 
-    struct cell_t;
-    struct expression_t {
-        enum class op_t : char {
-            plus = '+', minus = '-', none = '\0'
-        };
-        enum class expr_type_t : uint8_t {
-            none, ref, expr
-        };
-
-        expr_type_t type_;
-        std::string str_val_;
-
-        union val_t {
-            struct {
-                op_t op_;
-                std::shared_ptr<expression_t> left_;
-                std::shared_ptr<expression_t> right_;
-            } a_;
-            cell_t* ref_;
-            val_t() {}
-            ~val_t() {}
-        } val_;
-
-        expression_t() : type_(expr_type_t::none), str_val_(), val_()
-        {
-            val_.ref_ = nullptr;
-        }
-        ~expression_t() {}
-
+    enum class status_t {
+        Undefined, Calculating, Caclulated
     };
-
     struct cell_t {
+        std::string expression_;
         int64_t value_;
-        std::shared_ptr<expression_t> expr_;
-        bool evaluated_;
+        status_t status_;
 
-        cell_t () : value_(), expr_(), evaluated_(false) {}
-        bool evaluate();
+        cell_t() : expression_(), value_(), status_(status_t::Undefined) {}
 
-        friend std::istream& operator >> (std::istream& in, cell_t& cell)
+        friend std::istream& operator>>(std::istream& in, cell_t& cell)
         {
-            char ch(0);
-            cell.evaluated_ = false;
-
-            while (in >> ch) {
-                if (not cell.expr_ && std::isdigit(ch)) {
-                    cell.value_ *= 10;
-                    cell.value_ += ch - '0';
-                    cell.evaluated_ = true;
-                } else {
-                    if (not cell.expr_) {
-                        cell.expr_ = std::make_shared<expression_t>();
-                    }
-
-                    cell.expr_->str_val_.push_back(ch);
-                }
-            }
-
+            std::getline(in, cell.expression_);
+            cell.value_ = 0;
+            cell.status_ = status_t::Undefined;
             return in;
         }
     };
 
-    typedef std::array < cell_t, ('9' - '0') > row_t;
-    typedef std::array < row_t, ('T' - 'A') > spread_t;
+    typedef std::array < cell_t, ('9' - '0' + 1) > row_t;
+    typedef std::array < row_t, ('T' - 'A' + 1) > spread_t;
 
-    class solution {
+    class solution_t {
         uint16_t n_rows_, n_cols_;
         bool broken_;
+        spread_t spread_;
     public:
-        static spread_t spread_;
-        solution() : n_rows_(), n_cols_(), broken_() { }
+        solution_t() : n_rows_(), n_cols_(), broken_(), spread_() { }
 
-        friend std::istream& operator >>(std::istream& in, solution& sol);
-        friend std::ostream& operator <<(std::ostream& out, const solution& sol);
+        friend std::istream& operator >>(std::istream& in, solution_t& sol);
+        friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
         operator bool() const
         {
             return n_rows_ != 0 && n_cols_ != 0;
         }
 
-        solution& operator()();
+        solution_t& operator()();
     private:
+        int64_t calculate(const size_t row, const size_t col, bool& broken);
     };
 
-    std::istream& operator >> (std::istream& in, solution& sol)
+    std::istream& operator >> (std::istream& in, solution_t& sol)
     {
         if (in >> sol.n_rows_ >> sol.n_cols_) {
             if (sol) {
+                in.ignore();
                 sol.broken_ = false;
-                std::string line;
-                std::getline(in, line);
-                std::generate_n(sol.spread_.begin(), sol.n_rows_, [&]() {
-                    row_t row;
-                    std::generate_n(row.begin(), sol.n_cols_, [&]() {
-                        cell_t cell;
-                        std::getline(in, line);
-                        std::stringstream line_stream(line);
-                        line_stream >> cell;
-                        return cell;
-                    });
-                    return row;
-                });
+                for (size_t row = 0; row < sol.n_rows_; row++) {
+                	for (size_t col = 0; col < sol.n_cols_; col++) {
+                		in >> sol.spread_[row][col];
+                	}
+                }
             }
         }
 
         return in;
     }
 
-    std::ostream& operator << (std::ostream& out, const solution& sol)
+    std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
         if (not sol.broken_) {
-            out << std::setw(2) << ' ';
+            out << " ";
 
-            for (auto ch : "0123456789") {
-                if ((ch - '0') >= sol.n_cols_) {
-                    break;
-                }
-
-                out << std::setw(5) << std::right << ch;
+            for (size_t col = 0; col < sol.n_cols_; col++) {
+                out << std::setw(6) << col;
             }
 
             out << std::endl;
-            uint16_t n_rows(0);
 
-            for (auto& row : sol.spread_) {
-                if (n_rows >= sol.n_rows_) {
-                    break;
-                }
+            for (size_t row = 0; row < sol.n_rows_; row++) {
+                out << static_cast<char>('A' + row);
 
-                out << std::setw(2) << std::left << static_cast<char>('A' + n_rows);
-                uint16_t n_cols(0);
-
-                for (auto& cell : row) {
-                    if (n_cols >= sol.n_cols_) {
-                        break;
-                    }
-
-                    out << std::setw(5) << std::right << cell.value_;
-                    n_cols++;
+                for (size_t col = 0; col < sol.n_cols_; col++) {
+                    out << std::setw(6) << sol.spread_[row][col].value_;
                 }
 
                 out << std::endl;
-                n_rows++;
             }
         } else {
-            uint16_t n_rows(0);
-
-            for (auto& row : sol.spread_) {
-                if (n_rows >= sol.n_rows_) {
-                    break;
-                }
-
-                uint16_t n_cols(0);
-
-                for (auto& cell : row) {
-                    if (n_cols >= sol.n_cols_) {
-                        break;
+            for (size_t row = 0; row < sol.n_rows_; row++) {
+                for (size_t col = 0; col < sol.n_cols_; col++) {
+                    if (sol.spread_[row][col].status_ == status_t::Calculating) {
+                        out << static_cast<char>('A' + row) << col << ": " << sol.spread_[row][col].expression_ << std::endl;
                     }
-
-                    if (not cell.evaluated_ && cell.expr_ != nullptr) {
-                        out << static_cast<char>('A' + n_rows) << static_cast<char>('0' + n_cols) << ": " << cell.expr_->str_val_ << std::endl;
-                    }
-
-                    n_cols++;
                 }
-
-                n_rows++;
             }
         }
 
         return out;
     }
 
-    solution& solution::operator()()
+    solution_t& solution_t::operator()()
     {
-        uint16_t n_rows(0);
-
-        for (auto& row : spread_) {
-            if (broken_ || ++n_rows >= n_rows_) {
-                break;
-            }
-
-            uint16_t n_cols(0);
-
-            for (auto& cell : row) {
-                if (broken_ || ++n_cols >= n_cols_) {
-                    break;
-                }
-
-                if (not cell.evaluated_) {
-                    if (not cell.evaluate()) {
-                        broken_ = true;
-                        break;
-                    }
-                }
+        for (size_t row = 0; row < n_rows_; row++) {
+            for (size_t col = 0; col < n_cols_; col++) {
+            	bool broken(false);
+                calculate (row, col, broken);
+                broken_ |= broken;
             }
         }
 
         return *this;
     }
 
-    solution g_sol;
-
-    std::shared_ptr<expression_t> term(std::string::iterator& str)
+    int64_t solution_t::calculate(const size_t row, const size_t col, bool& broken)
     {
-        auto t = std::make_shared<expression_t>();
-        uint16_t row = (*str++) - 'A';
-        uint16_t col = (*str++) - '0';
-        t->val_.ref_ = &g_sol.spread_ [row][col];
-        t->type_ = expression_t::expr_type_t::ref;
-        return t;
-    }
+        switch (spread_[row][col].status_) {
+        case status_t::Caclulated:
+            return spread_[row][col].value_;
+            break;
 
-    void parse(std::shared_ptr<expression_t>& root)
-    {
-        auto str = root->str_val_.begin();
-        auto t = term(str);
+        case status_t::Calculating:
+            broken = true;
+            return 0;
 
-        if (str != root->str_val_.end()) {
-            root->val_.a_.left_ = t;
-            root->val_.a_.op_ = static_cast<expression_t::op_t>(*str++);
-            root->val_.a_.right_ = term(str);
-            root->type_ = expression_t::expr_type_t::expr;
-        } else {
-            root->val_.ref_ = t->val_.ref_;
-            root->type_ = expression_t::expr_type_t::ref;
+        default: {
+            spread_[row][col].status_ = status_t::Calculating;
+            auto ch = spread_[row][col].expression_.begin();
+            bool sign(true);
+
+            while (ch != spread_[row][col].expression_.end() && *ch != ' ') {
+                int64_t term(0);
+
+                switch (*ch) {
+                case '+':
+                    sign = true;
+                    ++ch;
+                    break;
+
+                case '-':
+                    sign = false;
+                    ++ch;
+                    break;
+
+                default:
+                    if (std::isdigit(*ch)) {
+                        while (std::isdigit(*ch)) {
+                            term *= 10;
+                            term += *(ch++) - '0';
+                        }
+                    } else {
+                        size_t refRow = *(ch++) - 'A';
+                        size_t refCol = *(ch++) - '0';
+                        term = calculate(refRow, refCol, broken);
+
+                        if (broken) {
+                            return 0;
+                        }
+                    }
+
+                    break;
+                }
+
+                spread_[row][col].value_ += sign ? term : -term;
+            }
+
+            spread_[row][col].status_ = status_t::Caclulated;
+            return spread_[row][col].value_;
+        }
+        break;
         }
     }
 
-    bool cell_t::evaluate()
-    {
-        if (expr_->type_ == expression_t::expr_type_t::none) {
-            parse(expr_);
-        }
-
-        if (expr_->type_ == expression_t::expr_type_t::expr) {
-            evaluated_ = true;
-
-            if (not expr_->val_.a_.left_->val_.ref_->evaluated_) {
-                expr_->val_.a_.left_->val_.ref_->evaluate();
-            }
-
-            if (not expr_->val_.a_.right_->val_.ref_->evaluated_) {
-                expr_->val_.a_.right_->val_.ref_->evaluate();
-            }
-
-            switch (expr_->val_.a_.op_) {
-            case expression_t::op_t::plus:
-                value_ = expr_->val_.a_.left_->val_.ref_->value_ + expr_->val_.a_.right_->val_.ref_->value_;
-                return true;
-
-            case expression_t::op_t::minus:
-                value_ = expr_->val_.a_.left_->val_.ref_->value_ - expr_->val_.a_.right_->val_.ref_->value_;
-                return true;
-
-            default:
-                break;
-            }
-        } else if (expr_->type_ == expression_t::expr_type_t::ref) {
-            if (expr_->val_.ref_ == this) {
-                return false;
-            }
-
-            value_ = expr_->val_.ref_->value_;
-            evaluated_ = true;
-            return true;
-        }
-
-        return false;
-    }
-
-    spread_t solution::spread_;
 }
 
 void U215::operator()() const
 {
+    solution_t sol;
 
-    while (std::cin >> g_sol && g_sol) {
-        std::cout << g_sol() << std::endl;
+    while (std::cin >> sol && sol) {
+        std::cout << sol() << std::endl;
     }
 }
+
+/*
+ #include <iostream>
+#include <string>
+#include <cctype>
+#include <iomanip>
+
+using namespace std;
+
+enum Status
+{
+    UNKNOWN, CALCULATED, CALCULATING
+};
+
+bool currentIsValid;
+
+Status statuses[25][15];
+string expressions[25][15];
+int value[25][15];
+
+int Calculate(int r, int c)
+{
+    if (statuses[r][c] == CALCULATED)
+    {
+        return value[r][c];
+    }
+    else if (statuses[r][c] == CALCULATING)
+    {
+        currentIsValid = false;
+        return 0;
+    }
+
+    statuses[r][c] = CALCULATING;
+    value[r][c] = 0;
+
+    bool isSubtract = false;
+    const string& expr = expressions[r][c];
+    for (int pos = 0; pos < expr.size(); ++pos)
+    {
+        // May have trailing blanks
+        if (expr[pos] == ' ')
+            break;
+
+        int toAdd = 0;
+        if (expr[pos] == '-')
+        {
+            isSubtract = true;
+            continue;
+        }
+        else if (expr[pos] == '+')
+            continue;
+        else if (isdigit(expr[pos]))
+        {
+            while (pos < expr.size() && isdigit(expr[pos]))
+            {
+                toAdd = toAdd * 10 + expr[pos] - '0';
+                ++pos;
+            }
+            // To counteract having gone past
+            --pos;
+        }
+        else
+        {
+            int newR = expr[pos] - 'A';
+            ++pos;
+            int newC = expr[pos] - '0';
+
+            toAdd = Calculate(newR, newC);
+
+            if (!currentIsValid)
+            {
+                return 0;
+            }
+        }
+
+        if (isSubtract)
+            toAdd *= -1;
+        value[r][c] += toAdd;
+        isSubtract = false;
+    }
+
+    statuses[r][c] = CALCULATED;
+    return value[r][c];
+}
+
+int main()
+{
+    int R, C;
+    while (cin >> R >> C, R)
+    {
+        cin.ignore();
+
+        for (int r = 0; r < R; ++r)
+        {
+            for(int c = 0; c < C; ++c)
+            {
+                getline(cin, expressions[r][c]);
+                statuses[r][c] = UNKNOWN;
+            }
+        }
+
+        bool overallIsValid = true;
+        for (int r = 0; r < R; ++r)
+        {
+            for(int c = 0; c < C; ++c)
+            {
+                currentIsValid = true;
+
+                Calculate(r, c);
+                overallIsValid &= currentIsValid;
+            }
+        }
+
+        if (overallIsValid)
+        {
+            cout << " ";
+            for (int c = 0; c < C; ++c)
+            {
+                cout << setw(6) << c;
+            }
+            cout << '\n';
+
+            for (int r = 0; r < R; ++r)
+            {
+                cout << char('A' + r);
+
+                for (int c = 0; c < C; ++c)
+                {
+                    cout << setw(6) << value[r][c];
+                }
+                cout << '\n';
+            }
+        }
+        else
+        {
+            for (int r = 0; r < R; ++r)
+            {
+                for (int c = 0; c < C; ++c)
+                {
+                    if (statuses[r][c] == CALCULATING)
+                        cout << char('A' + r) << c << ": " << expressions[r][c] << '\n';
+                }
+            }
+        }
+
+        cout << '\n';
+    }
+}
+
+ */
