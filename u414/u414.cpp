cdc1f73893dd1d4af660df1f72ca78da8eb02229
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-02-18 19:20:37 +0100

    u414

--- u414/u414.cpp
+++ u414/u414.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u414.h"
@@ -17,52 +17,98 @@
 #include <limits>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U414 instance;
-	instance();
+    U414 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : n_(), surface_(), sum_() {}
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+        operator bool()
+        {
+            return n_ > 0;
+        }
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+        solution_t& operator()();
 
-private:
-};
+        friend std::istream& operator>>(std::istream& in, solution_t& sol);
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+        friend std::ostream& operator<<(std::ostream& out, const solution_t& sol);
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+    private:
+        size_t n_;
+        std::vector<std::string> surface_;
+        int64_t sum_;
+    };
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+    solution_t& solution_t::operator()()
+    {
+        int64_t shift(std::numeric_limits<int64_t>::max());
+        std::vector<int64_t> pattern(n_);
+        auto pat(pattern.begin());
+
+        for (auto& line : surface_) {
+            auto ptr(line.begin());
+
+            for (; ptr != line.end() && *ptr == 'X'; ++ptr);
+
+            auto left(std::distance(line.begin(), ptr));
+
+            for (; ptr != line.end() && *ptr != 'X'; ++ptr);
+
+            auto right(std::distance(line.begin(), ptr));
+            *pat = right - left;
+
+            if (*pat < shift) {
+                shift = *pat;
+            }
+
+            ++pat;
+        }
+
+        sum_ = std::accumulate(pattern.begin(), pattern.end(), 0, [&](const int64_t old, const int64_t val) {
+            return old + val - shift;
+        });
+        return *this;
+    }
+
+    std::istream& operator>>(std::istream& in, solution_t& sol)
+    {
+        sol.surface_.clear();
+
+        if (in >> sol.n_) {
+            in.ignore();
+            sol.surface_.reserve(sol.n_);
+            std::string line;
+            std::generate_n(std::back_inserter(sol.surface_), sol.n_, [&]() {
+                std::getline(in, line);
+                return line;
+            });
+        }
+
+        return in;
+    }
+
+    std::ostream& operator<<(std::ostream& out, const solution_t& sol)
+    {
+        out << sol.sum_;
+        return out;
+    }
 
 }
 
 void U414::operator()() const
 {
     solution_t sol;
+
     while (std::cin >> sol && sol) {
         std::cout << sol() << std::endl;
     }
