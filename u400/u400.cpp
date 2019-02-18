3cd4bc803dd9c5d907e95d01093d118b98ac532f
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-02-18 19:13:00 +0100

    u400

--- u400/u400.cpp
+++ u400/u400.cpp
@@ -27,48 +27,88 @@ void __cdecl invoke()
     U400 instance;
     instance();
 }
-void U400::operator()() const
-{
-    uint32_t N;
-    std::vector<std::string> names;
-    std::string line;
-    std::string separator(60, '-');
 
-    while (std::cin >> N && N > 0) {
-        std::cout << separator << std::endl;
-        names.clear();
-        names.reserve(N);
-        std::getline(std::cin, line);
-        size_t longest(std::numeric_limits<size_t>::min());
-        std::generate_n(std::back_inserter(names), N, [&]() -> std::string {
-            std::getline(std::cin, line);
-            longest = std::max(longest, line.size());
-            return line; });
-        std::sort(names.begin(), names.end());
-        size_t ncolumns(std::max(60 / longest, size_t(1)));
+namespace {
 
-        while ((ncolumns - 1) * (longest + 2) + longest > 60) {
-            ncolumns--;
+    class solution_t {
+    public:
+        solution_t() : n_(), names_(), nrows_(), col_width_() {}
+
+        operator bool()
+        {
+            return true;
         }
 
-        size_t nrows(names.size() / ncolumns);
+        solution_t& operator()();
 
-        if (nrows * ncolumns < names.size()) {
-            nrows++;
+        friend std::istream& operator>>(std::istream& in, solution_t& sol);
+
+        friend std::ostream& operator<<(std::ostream& out, const solution_t& sol);
+
+    private:
+        size_t n_;
+        std::vector<std::string> names_;
+        size_t nrows_;
+        size_t col_width_;
+    };
+
+    solution_t& solution_t::operator()()
+    {
+        std::sort(names_.begin(), names_.end());
+        col_width_ = std::accumulate(names_.begin(), names_.end(), 0, [](const size_t prev, const std::string & str) {
+            return prev < str.length() ? str.length() : prev;
+        });
+        col_width_ += 2;
+
+        if (col_width_ > 60) {
+            col_width_ = 60;
         }
 
-        for (size_t idx = 0; idx < nrows; idx++) {
-            for (size_t column = 0; column < ncolumns; column++) {
-                if (column * nrows + idx < names.size()) {
-                    std::cout.setf(std::ios::left);
-                    auto shift = (column == ncolumns - 1)
-                                 || ((column + 1) * nrows + idx >= names.size());
-                    std::cout << std::setw(longest + (shift ? 0 : 2))
-                              << names[column * nrows + idx];
+        auto ncols = col_width_ == 31 ? 2 : 60 / col_width_;
+        nrows_ = n_ / ncols + (n_ % ncols != 0 ? 1 : 0);
+        return *this;
+    }
+
+    std::istream& operator>>(std::istream& in, solution_t& sol)
+    {
+        sol.names_.clear();
+
+        if (in >> sol.n_) {
+            sol.names_.reserve(sol.n_);
+            std::istream_iterator<std::string> sin(in);
+            std::copy_n(sin, sol.n_, std::back_inserter(sol.names_));
+        }
+
+        return in;
+    }
+
+    std::ostream& operator<<(std::ostream& out, const solution_t& sol)
+    {
+        out << std::string(60, '-') << std::endl;
+
+        for (size_t i = 0; i < sol.nrows_; ++i) {
+            for (size_t j = 0; i + j < sol.n_; j += sol.nrows_) {
+                if (j > 0) {
+                    out << "  ";
                 }
+
+                out << std::left << std::setw(sol.col_width_ - 2) << sol.names_[i + j];
             }
 
-            std::cout << std::endl;
+            out << std::endl;
         }
+
+        return out;
+    }
+
+
+}  // namespace
+
+void U400::operator()() const
+{
+    solution_t sol;
+
+    while (std::cin >> sol && sol) {
+        std::cout << sol() << std::endl;
     }
 }
