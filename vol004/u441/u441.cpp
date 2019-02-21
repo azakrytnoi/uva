570c83acc324839bc33816c6297d6938478841de
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-02-21 20:59:44 +0100

    done

--- vol004/u441/u441.cpp
+++ vol004/u441/u441.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u441.h"
@@ -17,53 +17,97 @@
 #include <limits>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U441 instance;
-	instance();
+    U441 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : set_(), selected_(6) { }
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+        friend std::istream& operator >>(std::istream& in, solution_t& sol);
+        friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+        operator bool() const
+        {
+            return not set_.empty();
+        }
+        solution_t& operator()(std::ostream& out);
 
-private:
-};
+    private:
+        std::vector<uint16_t> set_;
+        std::vector<uint16_t> selected_;
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+        void select_and_print(size_t selected, size_t pos, std::ostream& out);
+    };
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+    std::istream& operator >> (std::istream& in, solution_t& sol)
+    {
+        sol.set_.clear();
+        size_t n;
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+        if (in >> n && n > 0) {
+            sol.set_.reserve(n);
+            std::istream_iterator<uint16_t> sin(in);
+            std::copy_n(sin, n, std::back_inserter(sol.set_));
+        }
+
+        return in;
+    }
+
+    std::ostream& operator << (std::ostream& out, const solution_t& /*sol*/)
+    {
+        return out;
+    }
+
+    solution_t& solution_t::operator()(std::ostream& out)
+    {
+        std::sort(set_.begin(), set_.end());
+        select_and_print(0, 0, out);
+        return *this;
+    }
+
+    void solution_t::select_and_print(size_t selected, size_t pos, std::ostream& out)
+    {
+        if (selected + set_.size() - pos < 6) {
+            return;
+        }
+
+        if (pos == set_.size()) {
+            if (selected == 6) {
+                auto sel = selected_.begin();
+                out << *(sel++);
+                std::for_each(sel, selected_.end(), [&](const uint16_t num) {
+                    out << ' ' << num;
+                });
+                out << std::endl;
+            }
+
+            return;
+        }
+
+        if (selected < 6) {
+            selected_[selected] = set_[pos];
+            select_and_print(selected + 1, pos + 1, out);
+        }
+
+        select_and_print(selected, pos + 1, out);
+    }
 
 }
 
 void U441::operator()() const
 {
     solution_t sol;
+
     while (std::cin >> sol && sol) {
-        std::cout << sol() << std::endl;
+        std::cout << sol(std::cout) << std::endl;
     }
 }
