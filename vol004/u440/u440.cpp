771debc70e3df504f636e8ff95a34ee8c60f3d56
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-03-01 19:44:14 +0100

    done

--- vol004/u440/u440.cpp
+++ vol004/u440/u440.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u440.h"
@@ -17,52 +17,98 @@
 #include <limits>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U440 instance;
-	instance();
+    U440 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : DP_(N_), n_(), m_()
+        {
+            auto backtrace = [](size_t n, size_t k) -> bool {
+                std::vector<size_t> link(N_ + 1);
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+                for (size_t i = 2; i < n; ++i)
+                {
+                    link[i] = i + 1;
+                }
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+                link[n] = 2;
+                size_t head(2), prev(n), last(0);
 
-private:
-};
+                for (size_t i = 1; i < n; ++i)
+                {
+                    for (size_t j = 1; j < k; ++j) {
+                        prev = head, head = link[head];
+                    }
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+                    last = head;
+                    link[prev] = link[head];
+                    head = link[head];
+                }
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+                return last == 2;
+            };
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+            for (size_t i = 3; i < N_; ++i) {
+                size_t count(2);
+
+                while (not backtrace(i, count)) {
+                    ++count;
+                }
+
+                DP_[i] = count;
+            }
+        }
+
+        operator bool()
+        {
+            return n_ != 0;
+        }
+
+        solution_t& operator()();
+
+        friend std::istream& operator>>(std::istream& in, solution_t& sol);
+
+        friend std::ostream& operator<<(std::ostream& out, const solution_t& sol);
+
+    private:
+        const static size_t N_ = 150;
+        std::vector<size_t> DP_;
+        size_t n_, m_;
+    };
+
+    solution_t& solution_t::operator()()
+    {
+        m_ = DP_[n_];
+        return *this;
+    }
+
+    std::istream& operator>>(std::istream& in, solution_t& sol)
+    {
+        in >> sol.n_;
+        return in;
+    }
+
+    std::ostream& operator<<(std::ostream& out, const solution_t& sol)
+    {
+        out << sol.m_;
+        return out;
+    }
 
 }
 
 void U440::operator()() const
 {
     solution_t sol;
+
     while (std::cin >> sol && sol) {
         std::cout << sol() << std::endl;
     }
