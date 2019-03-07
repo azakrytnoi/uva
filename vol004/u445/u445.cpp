043434e9f141a64014d4ed20fa34f5589679fa08
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-03-07 10:29:45 +0100

    done

--- vol004/u445/u445.cpp
+++ vol004/u445/u445.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u445.h"
@@ -19,53 +19,99 @@
 #include <limits>
 
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U445 instance;
-	instance();
+    U445 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : source_(), maze_() { }
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+        friend std::istream& operator >>(std::istream& in, solution_t& sol);
+        friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+        operator bool() const
+        {
+            return not source_.empty();
+        }
+        solution_t& operator()();
 
-private:
-};
+    private:
+        std::vector<std::string> source_;
+        std::vector<std::string> maze_;
+    };
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+    std::istream& operator >> (std::istream& in, solution_t& sol)
+    {
+        sol.source_.clear();
+        std::string line;
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+        while (std::getline(in, line) && not line.empty()) {
+            sol.source_.push_back(line);
+        }
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+        return in;
+    }
+
+    std::ostream& operator << (std::ostream& out, const solution_t& sol)
+    {
+        std::ostream_iterator<std::string> sout(out, "\n");
+        std::copy(sol.maze_.begin(), sol.maze_.end(), sout);
+        return out;
+    }
+
+    solution_t& solution_t::operator()()
+    {
+        maze_.clear();
+
+        for (auto& src : source_) {
+            size_t n(0);
+            std::string line;
+
+            for (auto ch : src) {
+                switch (ch) {
+                case '!':
+                    maze_.push_back(line);
+                    line.clear();
+                    n = 0;
+                    break;
+
+                default:
+                    if (std::isdigit(ch)) {
+                        n += ch - '0';
+                    } else {
+                        line += std::string(n, ch == 'b' ? ' ' : ch);
+                        n = 0;
+                    }
+
+                    break;
+                }
+            }
+
+            maze_.push_back(line);
+        }
+
+        return *this;
+    }
 
 }
 
 void U445::operator()() const
 {
     solution_t sol;
-    while (std::cin >> sol && sol) {
-        std::cout << sol() << std::endl;
-    }
+
+    do {
+        std::cin >> sol;
+
+        if (sol) {
+            std::cout << sol() << std::endl;
+        }
+    } while (sol);
 }
