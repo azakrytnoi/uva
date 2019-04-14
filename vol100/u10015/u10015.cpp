4e1d4970dce95cb2a7b4b40f2e49f5cf901eb53d
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-04-14 19:51:32 +0200

    done

--- vol100/u10015/u10015.cpp
+++ vol100/u10015/u10015.cpp
@@ -1,8 +1,8 @@
 #ifdef _WIN32
-#define UVA_API_EXPORT __declspec(dllexport)
+    #define UVA_API_EXPORT __declspec(dllexport)
 #else
-#define __cdecl
-#define UVA_API_EXPORT
+    #define __cdecl
+    #define UVA_API_EXPORT
 #endif
 
 #include "u10015.h"
@@ -18,53 +18,72 @@
 #include <memory>
 #include <limits>
 
+#include "prime.h"
+
 extern "C" {
-	UVA_API_EXPORT void __cdecl invoke();
+    UVA_API_EXPORT void __cdecl invoke();
 }
 
 void __cdecl invoke()
 {
-	U10015 instance;
-	instance();
+    U10015 instance;
+    instance();
 }
 
-namespace
-{
+namespace {
 
-class solution_t
-{
-public:
-    solution_t() { }
+    class solution_t {
+    public:
+        solution_t() : primes_(), n_(), res_() { }
 
-    friend std::istream& operator >>(std::istream& in, solution_t& sol);
-    friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
+        friend std::istream& operator >>(std::istream& in, solution_t& sol);
+        friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
-    operator bool() const { return true; }
-    solution_t& operator()();
+        operator bool() const
+        {
+            return n_ > 0;
+        }
+        solution_t& operator()();
 
-private:
-};
+    private:
+        math::sieve_t<40000> primes_;
+        uint64_t n_;
+        uint64_t res_;
 
-std::istream& operator >> (std::istream& in, solution_t& sol)
-{
-  return in;
-}
+        uint64_t f(uint64_t n, uint64_t k)
+        {
+            if (n == 1) {
+                return 1;
+            }
 
-std::ostream& operator << (std::ostream& out, const solution_t& sol)
-{
-  return out;
-}
+            return ((f(n - 1, k) + k - 1) % n) + 1;
+        };
+    };
 
-solution_t& solution_t::operator()()
-{
-  return *this;
-}
+    std::istream& operator >> (std::istream& in, solution_t& sol)
+    {
+        in >> sol.n_;
+        return in;
+    }
+
+    std::ostream& operator << (std::ostream& out, const solution_t& sol)
+    {
+        out << sol.res_;
+        return out;
+    }
+
+    solution_t& solution_t::operator()()
+    {
+        res_ = f(n_, primes_[n_]);
+        return *this;
+    }
 
 }
 
 void U10015::operator()() const
 {
     solution_t sol;
+
     while (std::cin >> sol && sol) {
         std::cout << sol() << std::endl;
     }
--- vol100/u10015/u10015.h
+++ vol100/u10015/u10015.h
@@ -1,17 +1,16 @@
 #pragma once
 
-class U10015
-{
+class U10015 {
 public:
-	static const char * libname()
-	{
-		return "u10015";
-	}
+    static const char* libname()
+    {
+        return "u10015";
+    }
     U10015() {}
 
     void operator()() const;
 };
 
 #ifdef POPULATE_CACHE
-populate <U10015> pu10015;
+    populate <U10015> pu10015;
 #endif
