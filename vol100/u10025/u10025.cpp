3657b2f6036ba64222504cf6761bb86d43da3194
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-04-24 15:30:09 +0200

    done

--- vol100/u10025/u10025.cpp
+++ vol100/u10025/u10025.cpp
@@ -32,32 +32,51 @@ namespace {
 
     class solution_t {
     public:
-        solution_t() { }
+        solution_t() : N_(std::numeric_limits<size_t>::max()), n_(), k_() { }
 
         friend std::istream& operator >>(std::istream& in, solution_t& sol);
         friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
-        operator bool() const
+        operator bool()
         {
-            return true;
+            return N_-- > 0;
         }
         solution_t& operator()();
 
     private:
+        size_t N_;
+        int64_t n_, k_;
     };
 
     std::istream& operator >> (std::istream& in, solution_t& sol)
     {
+        if (sol.N_ == std::numeric_limits<size_t>::max())
+        {
+            in >> sol.N_;
+        }
+
+        in >> sol.n_;
         return in;
     }
 
     std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
+        out << sol.k_ << std::endl;
         return out;
     }
 
     solution_t& solution_t::operator()()
     {
+        n_ = std::abs(n_);
+
+        for (k_ = 1;; ++k_)
+        {
+            if (k_ * (k_ + 1) / 2 >= n_ && (k_ * (k_ + 1) / 2 - n_) % 2 == 0)
+            {
+                break;
+            }
+        }
+
         return *this;
     }
 
