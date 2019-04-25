23ed7b952de417eba648b95b0ea3c8c082b45b04
Aleksey Zakrytnoi <azakrytnoi@gmail.com>
2019-04-25 17:08:32 +0200

    input ready

--- vol100/u10027/u10027.cpp
+++ vol100/u10027/u10027.cpp
@@ -11,12 +11,15 @@
 #include <iomanip>
 #include <vector>
 #include <map>
+#include <set>
 #include <string>
 #include <algorithm>
 #include <iterator>
 #include <numeric>
 #include <memory>
 #include <limits>
+#include <regex>
+#include <list>
 
 extern "C" {
     UVA_API_EXPORT void __cdecl invoke();
@@ -32,27 +35,66 @@ namespace {
 
     class solution_t {
     public:
-        solution_t() { }
+        solution_t() : N_(std::numeric_limits<size_t>::max()), initial_(), transitions_(), language_() { }
 
         friend std::istream& operator >>(std::istream& in, solution_t& sol);
         friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
-        operator bool() const
+        operator bool()
         {
-            return true;
+            return N_-- > 0;
         }
         solution_t& operator()();
 
     private:
+        size_t N_;
+        std::string initial_;
+        std::map<std::string, std::list<std::string>> transitions_;
+        std::set<std::string> language_;
     };
 
     std::istream& operator >> (std::istream& in, solution_t& sol)
     {
+        if (sol.N_ == std::numeric_limits<size_t>::max())
+        {
+            in >> sol.N_;
+            in.ignore(2);
+        }
+
+        if (std::getline(in, sol.initial_))
+        {
+            sol.language_.clear();
+            sol.transitions_.clear();
+            sol.initial_.erase(sol.initial_.begin());
+            sol.initial_.erase(sol.initial_.end() - 1);
+            std::string temp;
+            std::regex pattern("\"([^\"]+)\"->\"([^\"]+)\"");
+
+            while (std::getline(in, temp) && not temp.empty())
+            {
+                std::smatch match;
+
+                if (std::regex_search(temp, match, pattern))
+                {
+                    sol.transitions_[match[1]].push_back(match[2]);
+                }
+            }
+        }
+
         return in;
     }
 
     std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
+        if (sol.language_.size() > 1000)
+        {
+            out << "Too many" << std::endl;
+        }
+        else
+        {
+            out << sol.language_.size() << std::endl;
+        }
+
         return out;
     }
 
@@ -67,8 +109,17 @@ void U10027::operator()() const
 {
     solution_t sol;
 
-    while (std::cin >> sol && sol)
+    while (true)
     {
-        std::cout << sol() << std::endl;
+        std::cin >> sol;
+
+        if (sol)
+        {
+            std::cout << sol() << std::endl;
+        }
+        else
+        {
+            break;
+        }
     }
 }
