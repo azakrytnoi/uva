cc91ba080251f23cfc41ab41a6c05d12ea0e5d4a
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-05-06 15:31:47 +0200

    done

--- vol100/u10063/u10063.cpp
+++ vol100/u10063/u10063.cpp
@@ -32,7 +32,7 @@ namespace {
 
     class solution_t {
     public:
-        solution_t() { }
+        solution_t() : src_(), tmp_(), ans_() { }
 
         friend std::istream& operator >>(std::istream& in, solution_t& sol);
         friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
@@ -44,21 +44,54 @@ namespace {
         solution_t& operator()();
 
     private:
+        std::string src_;
+        std::string tmp_;
+        std::vector<std::string> ans_;
+
+        void dfs(size_t idx);
     };
 
     std::istream& operator >> (std::istream& in, solution_t& sol)
     {
+        in >> sol.src_;
+        sol.ans_.clear();
         return in;
     }
 
     std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
+        std::ostream_iterator<std::string> sout (out, "\n");
+        std::copy(sol.ans_.begin(), sol.ans_.end(), sout);
         return out;
     }
 
     solution_t& solution_t::operator()()
     {
+        tmp_.clear();
+        tmp_.resize(src_.size());
+        dfs(0);
         return *this;
+    }
+
+    void solution_t::dfs(size_t idx)
+    {
+        if (idx == src_.size())
+        {
+            ans_.push_back(tmp_);
+            return;
+        }
+
+        for (auto i = idx; i >= 1; --i)
+        {
+            tmp_[i] = tmp_[i - 1];
+        }
+
+        for (auto i = 0u; i <= idx; ++i)
+        {
+            tmp_[i] = src_[idx];
+            dfs(idx + 1);
+            tmp_[i] = tmp_[i + 1];
+        }
     }
 
 }
