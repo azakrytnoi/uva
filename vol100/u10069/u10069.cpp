1b2fff77edc58378f7588b49270cbb886c75b4f0
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-05-10 15:31:54 +0200

    done

--- vol100/u10069/u10069.cpp
+++ vol100/u10069/u10069.cpp
@@ -18,6 +18,9 @@
 #include <memory>
 #include <limits>
 
+#include "biguint.h"
+
+
 extern "C" {
     UVA_API_EXPORT void __cdecl invoke();
 }
@@ -32,35 +35,76 @@ namespace {
 
     class solution_t {
     public:
-        solution_t() { }
+        solution_t() : N_(std::numeric_limits<size_t>::max()), main_(), sub_(), result_() { }
 
         friend std::istream& operator >>(std::istream& in, solution_t& sol);
         friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
-        operator bool() const
+        operator bool()
         {
-            return true;
+            return N_-- > 0;
         }
         solution_t& operator()();
 
     private:
+        size_t N_;
+        std::string main_, sub_;
+        math::uint_big_t result_;
+
+        typedef std::vector<std::vector<math::uint_big_t>> matrix_t;
+        math::uint_big_t count(size_t main_pos, size_t sub_pos, matrix_t& matrix);
     };
 
     std::istream& operator >> (std::istream& in, solution_t& sol)
     {
+        if (sol.N_ == std::numeric_limits<size_t>::max())
+        {
+            in >> sol.N_;
+        }
+
+        in >> sol.main_ >> sol.sub_;
         return in;
     }
 
     std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
+        out << sol.result_;
         return out;
     }
 
     solution_t& solution_t::operator()()
     {
+        matrix_t mat(std::vector<std::vector<math::uint_big_t>>(main_.size() + 1, std::vector<math::uint_big_t>(sub_.size() + 1)));
+        result_ = count(0u, 0u, mat);
         return *this;
     }
 
+    math::uint_big_t solution_t::count(size_t main_pos, size_t sub_pos, matrix_t& matrix)
+    {
+        if (sub_pos == sub_.length())
+        {
+            return 1;
+        }
+
+        if (main_pos == main_.length())
+        {
+            return 0;
+        }
+
+        if (matrix[main_pos][sub_pos] == 0)
+        {
+            auto cnt(count(main_pos + 1, sub_pos, matrix));
+
+            if (main_[main_pos] == sub_[sub_pos])
+            {
+                cnt += count(main_pos + 1, sub_pos + 1, matrix);
+            }
+
+            matrix[main_pos][sub_pos] = cnt;
+        }
+
+        return matrix[main_pos][sub_pos];
+    }
 }
 
 void U10069::operator()() const
