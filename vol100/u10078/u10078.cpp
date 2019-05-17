0975f5cdce43365b05d1f50fed8068b40f1a9455
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-05-17 10:13:36 +0200

    done

--- vol100/u10078/u10078.cpp
+++ vol100/u10078/u10078.cpp
@@ -18,6 +18,8 @@
 #include <memory>
 #include <limits>
 
+#include "point.h"
+
 extern "C" {
     UVA_API_EXPORT void __cdecl invoke();
 }
@@ -28,36 +30,99 @@ void __cdecl invoke()
     instance();
 }
 
+namespace geom {
+
+    std::istream& operator >> (std::istream& in, geom::point_t& point)
+    {
+        in >> point.x_ >> point.y_;
+        return in;
+    }
+
+};
+
 namespace {
 
     class solution_t {
     public:
-        solution_t() { }
+        solution_t() : points_(), result_() { }
 
         friend std::istream& operator >>(std::istream& in, solution_t& sol);
         friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
         operator bool() const
         {
-            return true;
+            return not points_.empty();
         }
         solution_t& operator()();
 
     private:
+        std::vector<geom::point_t> points_;
+        bool result_;
     };
 
     std::istream& operator >> (std::istream& in, solution_t& sol)
     {
+        sol.points_.clear();
+        size_t n(0);
+
+        if (in >> n && n > 0)
+        {
+            sol.points_.reserve(n);
+            std::istream_iterator<geom::point_t> pin(in);
+            std::copy_n(pin, n, std::back_inserter(sol.points_));
+        }
+
         return in;
     }
 
     std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
+        out << (sol.result_ ? "Yes" : "No");
         return out;
     }
 
     solution_t& solution_t::operator()()
     {
+        std::vector<geom::point_t> deltas;
+        deltas.reserve(points_.size());
+
+        for (auto i = 0u; i < points_.size(); ++i)
+        {
+            auto j ((i + 1) % points_.size());
+            deltas.push_back(points_[j] - points_[i]);
+        }
+
+        std::vector<double_t> t;
+        t.reserve(deltas.size());
+
+        for (auto i = 0u; i < deltas.size(); ++i)
+        {
+            auto j ((i + 1) % deltas.size());
+            t.push_back(deltas[i].x_ * deltas[j].y_ - deltas[i].y_ * deltas[j].x_);
+        }
+
+        auto check = [&]() -> bool
+        {
+            if (t[0] < 0)
+            {
+                for (auto i = 1u; i < t.size(); ++i)
+                    if (t[i] > 0)
+                    {
+                        return true;
+                    }
+            }
+            else
+            {
+                for (auto i = 1u; i < t.size(); ++i)
+                    if (t[i] < 0)
+                    {
+                        return true;
+                    }
+            }
+
+            return false;
+        };
+        result_ = check();
         return *this;
     }
 
