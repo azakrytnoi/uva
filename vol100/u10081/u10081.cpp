6a75b65cb3114ea7e5ef65187489665107c5ec35
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-05-20 13:34:20 +0200

    done

--- vol100/u10081/u10081.cpp
+++ vol100/u10081/u10081.cpp
@@ -17,6 +17,7 @@
 #include <numeric>
 #include <memory>
 #include <limits>
+#include <cmath>
 
 extern "C" {
     UVA_API_EXPORT void __cdecl invoke();
@@ -32,7 +33,7 @@ namespace {
 
     class solution_t {
     public:
-        solution_t() { }
+        solution_t() : k_(), n_(), res_() { }
 
         friend std::istream& operator >>(std::istream& in, solution_t& sol);
         friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
@@ -44,20 +45,41 @@ namespace {
         solution_t& operator()();
 
     private:
+        size_t k_, n_;
+        double_t res_;
     };
 
     std::istream& operator >> (std::istream& in, solution_t& sol)
     {
+        in >> sol.k_ >> sol.n_;
         return in;
     }
 
     std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
+        out << std::fixed << std::setprecision(5) << sol.res_;
         return out;
     }
 
     solution_t& solution_t::operator()()
     {
+        std::vector<std::vector<double_t>> dp(n_ + 1, std::vector <double_t>(k_ + 1, 0.0));
+        std::fill(dp[1].begin(), dp[1].end(), 1.0);
+
+        for (auto i = 2u; i <= n_; ++i)
+        {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][k_] = dp[i - 1][k_] + dp[i - 1][k_ - 1];
+
+            for (auto j = 1u; j < k_; j++)
+            {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j] + dp[i - 1][j + 1];
+            }
+        }
+
+        res_ = std::accumulate(dp[n_].begin(), dp[n_].end(), 0.0);
+        auto total (std::pow(k_ + 1, n_));
+        res_ = (res_ * 100.0) / total;
         return *this;
     }
 
--- vol100/u10082/u10082.cpp
+++ vol100/u10082/u10082.cpp
@@ -32,7 +32,7 @@ namespace {
 
     class solution_t {
     public:
-        solution_t() { }
+        solution_t() : digits_("`1234567890-="), letters_("QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./"), message_() { }
 
         friend std::istream& operator >>(std::istream& in, solution_t& sol);
         friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
@@ -44,20 +44,43 @@ namespace {
         solution_t& operator()();
 
     private:
+        const std::string digits_;
+        const std::string letters_;
+        std::string message_;
     };
 
     std::istream& operator >> (std::istream& in, solution_t& sol)
     {
+        std::getline(in, sol.message_);
         return in;
     }
 
     std::ostream& operator << (std::ostream& out, const solution_t& sol)
     {
+        out << sol.message_;
         return out;
     }
 
     solution_t& solution_t::operator()()
     {
+        std::transform(message_.begin(), message_.end(), message_.begin(), [&](auto ch) -> char
+        {
+            auto pos = std::find(digits_.begin(), digits_.end(), ch);
+
+            if (pos != digits_.end())
+            {
+                return *(--pos);
+            }
+
+            pos = std::find(letters_.begin(), letters_.end(), ch);
+
+            if (pos != letters_.end())
+            {
+                return *(--pos);
+            }
+
+            return ch;
+        });
         return *this;
     }
 
