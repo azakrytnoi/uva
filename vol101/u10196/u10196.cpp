6a11fb9ef7d755c70f2101655f87951f3da9c462
Aleksey Zakrytnoy <a.zakrytnoy@vwt.digital>
2019-06-04 18:58:52 +0200

    fixed #18

--- vol101/u10196/u10196.cpp
+++ vol101/u10196/u10196.cpp
@@ -15,6 +15,7 @@
 #include <iterator>
 #include <numeric>
 #include <limits>
+#include <functional>
 
 extern "C" {
     UVA_API_EXPORT void __cdecl invoke();
@@ -27,24 +28,55 @@ void __cdecl invoke()
 
 namespace {
 
-    class desk {
+    struct position_t : public std::pair<int, int>
+    {
+        position_t(int x = 0, int y = 0) : std::pair<int, int>(x, y) {}
+        position_t(size_t x = 0, size_t y = 0) : std::pair<int, int>(x, y) {}
+
+        bool within ()
+        {
+            return first >= 0 && second >= 0 && first <= 7 && second <= 7;
+        }
+
+        position_t& operator += (const position_t& rhs)
+        {
+            first += rhs.first, second += rhs.second;
+            return *this;
+        }
+
+        friend position_t operator + (const position_t& lhs, const position_t& rhs)
+        {
+            return {lhs.first + rhs.first, lhs.second + rhs.second};
+        }
+
+    };
+
+    typedef std::pair<position_t, std::function<void(position_t&)>> delta_t;
+
+    std::ostream& operator <<(std::ostream& out, const position_t& p)
+    {
+        out << '(' << p.first << ',' << p.second << ')';
+        return out;
+    }
+
+    class desk_t {
     public:
         std::vector<std::string> field_;
-        std::pair<int, int> wKing_;
-        std::pair<int, int> bKing_;
+        position_t wKing_;
+        position_t bKing_;
 
-        desk() :
-            field_(), wKing_(0, 0), bKing_(0, 0)
+        desk_t() :
+            field_(), wKing_{0, 0}, bKing_{0, 0}
         {
             field_.reserve(8);
         }
 
-        friend std::istream& operator >>(std::istream& in, desk& d)
+        friend std::istream& operator >>(std::istream& in, desk_t& d)
         {
             std::string line;
             d.field_.clear();
-            d.wKing_ = {0, 0 };
-            d.bKing_ = {0, 0 };
+            d.wKing_ = {0, 0};
+            d.bKing_ = {0, 0};
             std::generate_n(std::back_inserter(d.field_), 8, [&]()
             {
                 std::getline(in, line);
@@ -52,12 +84,12 @@ namespace {
 
                 if ((kPos = line.find('K')) != std::string::npos)
                 {
-                    d.wKing_ = {d.field_.size(), kPos };
+                    d.wKing_ = {d.field_.size(), kPos};
                 }
 
                 if ((kPos = line.find('k')) != std::string::npos)
                 {
-                    d.bKing_ = {d.field_.size(), kPos };
+                    d.bKing_ = {d.field_.size(), kPos};
                 }
 
                 return line;
@@ -66,51 +98,63 @@ namespace {
             return in;
         }
 
+        friend std::ostream& operator <<(std::ostream& out, const desk_t& d)
+        {
+            out << "-- " << d.bKing_ << ' ' << d.wKing_ << std::endl;
+            std::ostream_iterator<std::string> sout(out, "\n");
+            std::copy(d.field_.begin(), d.field_.end(), sout);
+            out << "----------------------" << std::endl;
+            return out;
+        }
+
         operator bool() const
         {
             return wKing_.first + wKing_.second + bKing_.first + bKing_.second != 0;
         }
     };
 
-    class solution {
+    class solution_t {
     public:
-        solution() :
+        solution_t() :
             desk_(), wKingCheck_(false), bKingCheck_(false)
         {}
 
-        friend std::istream& operator >>(std::istream& in, solution& sol);
-        friend std::ostream& operator <<(std::ostream& out, const solution& sol);
+        friend std::istream& operator >>(std::istream& in, solution_t& sol);
+        friend std::ostream& operator <<(std::ostream& out, const solution_t& sol);
 
         operator bool() const
         {
             return bool(desk_);
         }
 
-        solution& operator()();
+        solution_t& operator()();
 
     private:
-        desk desk_;
+        desk_t desk_;
         bool wKingCheck_;
         bool bKingCheck_;
 
-        bool traceDanger(const std::pair<int, int>& kPosition, int direction) const;
+        bool traceDanger(const position_t& kPosition, int direction) const;
+        bool traceDiagonal(const position_t& kPosition, int direction) const;
+        bool traceDirect(const position_t& kPosition, int direction) const;
+        bool traceKnight(const position_t& kPosition, int direction) const;
     };
 
-    solution& solution::operator ()()
+    solution_t& solution_t::operator ()()
     {
         wKingCheck_ = traceDanger(desk_.wKing_, -1);
         bKingCheck_ = traceDanger(desk_.bKing_, 1);
         return *this;
     }
 
-    std::istream& operator >>(std::istream& in, solution& sol)
+    std::istream& operator >>(std::istream& in, solution_t& sol)
     {
         sol.wKingCheck_ = sol.bKingCheck_ = false;
         in >> sol.desk_;
         return in;
     }
 
-    std::ostream& operator <<(std::ostream& out, const solution& sol)
+    std::ostream& operator <<(std::ostream& out, const solution_t& sol)
     {
         if (sol.wKingCheck_)
         {
@@ -128,19 +172,52 @@ namespace {
         return out;
     }
 
-    bool solution::traceDanger(const std::pair<int, int>& kPosition, const int direction) const
+    bool solution_t::traceDanger(const position_t& kPosition, const int direction) const
     {
-        bool check(false);
-        std::pair<int, int> checkPosition;
-        bool safe;
-        auto checkDiagonal = [&] ()
-        {
-            char figure;
+        return traceDiagonal(kPosition, direction) || traceDirect(kPosition, direction) || traceKnight(kPosition, direction);
+    }
 
-            switch (figure = desk_.field_[checkPosition.first][checkPosition.second])
+    bool solution_t::traceDiagonal(const position_t& kPosition, int direction) const
+    {
+        position_t checkPosition(0, 0);
+        bool safe, check(false);
+        auto testPosition = [&] ()
+        {
+            char figure (desk_.field_[checkPosition.first][checkPosition.second]);
+            // std::clog << desk_.field_[kPosition.first][kPosition.second] << kPosition << " -> " << figure << checkPosition << std::endl;
+
+            switch (figure)
             {
             case 'p':
             case 'P':
+            {
+                auto p_distance = [&]()
+                {
+                    return std::abs(checkPosition.first - kPosition.first) == 1;
+                };
+
+                if (p_distance())
+                {
+                    if (direction < 0 && figure == 'p' && kPosition.first > checkPosition.first)
+                    {
+                        check = true;
+                    }
+                    else if (direction > 0 && figure == 'P' && kPosition.first < checkPosition.first)
+                    {
+                        check = true;
+                    }
+                    else
+                    {
+                        safe = true;
+                    }
+                }
+                else
+                {
+                    safe = true;
+                }
+            }
+            break;
+
             case 'b':
             case 'B':
             case 'q':
@@ -174,7 +251,30 @@ namespace {
                 break;
             }
         };
-        auto checkDirect = [&]()
+
+        static const std::vector <position_t> deltas (
+        {
+            {-1, -1}, {1, -1}, {-1, 1}, {1, 1}
+        });
+
+        for (auto& delta : deltas)
+        {
+            for (safe = false, checkPosition = kPosition + delta;
+                    !check && !safe && checkPosition.within();
+                    checkPosition += delta)
+            {
+                testPosition();
+            }
+        }
+
+        return check;
+    }
+
+    bool solution_t::traceDirect(const position_t& kPosition, int direction) const
+    {
+        position_t checkPosition(0, 0);
+        bool safe, check(false);
+        auto testPosition = [&]()
         {
             char figure;
 
@@ -215,7 +315,30 @@ namespace {
                 break;
             }
         };
-        auto checkKnight = [&]()
+
+        static const std::vector<position_t> deltas (
+        {
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+        });
+
+        for (auto& delta : deltas)
+        {
+            for (safe = false, checkPosition = kPosition + delta;
+                    !check && !safe && checkPosition.within();
+                    checkPosition += delta)
+            {
+                testPosition();
+            }
+        }
+
+        return check;
+    }
+
+    bool solution_t::traceKnight(const position_t& kPosition, int direction) const
+    {
+        position_t checkPosition(0, 0);
+        bool check(false);
+        auto testPosition = [&]()
         {
             char figure;
 
@@ -234,145 +357,34 @@ namespace {
 
                 break;
 
-            case 'q':
-            case 'Q':
-            case 'r':
-            case 'R':
-            case 'p':
-            case 'P':
-            case 'b':
-            case 'B':
-            case 'k':
-            case 'K':
-                safe = true;
-                break;
-
-            case '.':
             default:
                 break;
             }
         };
 
-        for (safe = false, checkPosition = {kPosition.first - 1, kPosition.second - 1 };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.first--, checkPosition.second--)
+        static const std::vector<position_t> deltas (
         {
-            checkDiagonal();
-        }
+            {-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {1, 2}, {2, 1}, {-1, 2}, {-2, 1}
+        });
 
-        for (safe = false, checkPosition = {kPosition.first + 1, kPosition.second - 1 };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.first++, checkPosition.second--)
+        for (auto& delta : deltas)
         {
-            checkDiagonal();
-        }
 
-        for (safe = false, checkPosition = {kPosition.first - 1, kPosition.second + 1 };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.first--, checkPosition.second++)
-        {
-            checkDiagonal();
-        }
+            checkPosition = kPosition + delta;
 
-        for (safe = false, checkPosition = {kPosition.first + 1, kPosition.second + 1 };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.first++, checkPosition.second++)
-        {
-            checkDiagonal();
-        }
-
-        for (safe = false, checkPosition = {kPosition.first - 1, kPosition.second };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.first--)
-        {
-            checkDirect();
-        }
-
-        for (safe = false, checkPosition = {kPosition.first + 1, kPosition.second };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.first++)
-        {
-            checkDirect();
-        }
-
-        for (safe = false, checkPosition = {kPosition.first, kPosition.second - 1 };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.second--)
-        {
-            checkDirect();
-        }
-
-        for (safe = false, checkPosition = {kPosition.first, kPosition.second + 1 };
-                !check && !safe && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7;
-                checkPosition.second++)
-        {
-            checkDirect();
-        }
-
-        checkPosition = {kPosition.first - 2, kPosition.second - 1 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
-        }
-
-        checkPosition = {kPosition.first - 1, kPosition.second - 2 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
-        }
-
-        checkPosition = {kPosition.first + 1, kPosition.second - 2 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
-        }
-
-        checkPosition = {kPosition.first + 2, kPosition.second - 1 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
-        }
-
-        checkPosition = {kPosition.first + 2, kPosition.second + 1 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
-        }
-
-        checkPosition = {kPosition.first + 1, kPosition.second + 2 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
-        }
-
-        checkPosition = {kPosition.first - 1, kPosition.second + 2 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
-        }
-
-        checkPosition = {kPosition.first - 2, kPosition.second + 1 };
-
-        if (!check && checkPosition.first >= 0 && checkPosition.second >= 0 && checkPosition.first <= 7 && checkPosition.second <= 7)
-        {
-            checkKnight();
+            if (!check && checkPosition.within())
+            {
+                testPosition();
+            }
         }
 
         return check;
     }
-
 }
 
 void U10196::operator()() const
 {
-    solution sol;
+    solution_t sol;
     int gameNo(0);
 
     while (std::cin >> sol && sol)
